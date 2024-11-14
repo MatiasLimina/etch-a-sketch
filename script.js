53e1945c7f6ef065e6b3c3d79b228e4a82c704de
Matias <matiaslimina@gmail.com>
2024-11-14 10:42:49 -0300

    Make new functionsz delete old one
    Make a function that does create som columns and a function to create the slots of the grid,combining both function creates a grid.
    
    [Ticket: 4]

--- script.js
+++ script.js
@@ -1,14 +1,37 @@
 const container = document.querySelector(".container")
 console.log(container)
-function createDivs(){
+// function createDivs(){
 
-    for (let i = 0; i <= 256; i++){
+//     for (let i = 0; i < 256; i++){
         
-        const div = document.createElement("div");
-        div.classList.add(i);
-        div.classList.add("colorSlot")
-        container.appendChild(div);
-        console.log (div);
+//         const div = document.createElement("div");
+//         div.classList.add(i+1);
+//         div.classList.add("colorSlot")
+//         container.appendChild(div);
+//         console.log (div);
+//     }
+// }
+// createDivs();
+
+
+function createCol(){
+
+    for (let i = 0; i < 16; i++){
+        
+        const col = document.createElement("div");
+        col.classList.add("col");
+        container.appendChild(col);
+        createRow(col);
+}};
+
+function createRow(column) {
+    
+    for (let i = 0; i<16; i++){
+        const row = document.createElement("div");
+        row.classList.add("row");
+        column.appendChild(row);
     }
-}
-console.log(createDivs());
+    
+};
+
+console.log(createCol());
